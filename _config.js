5509c4c70d017fc869a2d706b1e000726a4b36b7
WallaceJames <osangojamess@gmail.com>
2024-07-09 22:57:30 +0300

    mongo db password update

--- _config.js
+++ _config.js
@@ -2,7 +2,7 @@ var config = {};
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://James:Osango%402024@ac-ezo0e17.mongodb.net/admin',
+    production: 'mongodb+srv://James:Osango@2024@moringa.vrybqbf.mongodb.net/admin',
     development: 'mongodb+srv://James:Osango@2024@moringa.vrybqbf.mongodb.net/',
     test: 'mongodb+srv://James:Osango@2024@moringa.vrybqbf.mongodb.net/'
 };
