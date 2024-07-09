1f7f9332d6892e216c5f95e9005601fa0309ce7c
WallaceJames <osangojamess@gmail.com>
2024-07-09 22:52:26 +0300

    updated file

--- _config.js
+++ _config.js
@@ -1,9 +1,11 @@
-var config = {}
+var config = {};
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://James:Osango@2024@moringa.vrybqbf.mongodb.net/',
+    production: 'mongodb+srv://James:Osango%402024@ac-ezo0e17.mongodb.net/admin',
     development: 'mongodb+srv://James:Osango@2024@moringa.vrybqbf.mongodb.net/',
-    test: 'mongodb+srv://James:Osango@2024@moringa.vrybqbf.mongodb.net/',
-}
+    test: 'mongodb+srv://James:Osango@2024@moringa.vrybqbf.mongodb.net/'
+};
+
 module.exports = config;
+
